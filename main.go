74545fbe8ed43a832d6f22c0e35ce61bf8946b7a
Longwei <lwlu@gizwits.com>
2022-08-30 13:09:05 +0800

    config: 更改配置文件

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"log"
 	"resume-server/apis"
 	"resume-server/conf"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
